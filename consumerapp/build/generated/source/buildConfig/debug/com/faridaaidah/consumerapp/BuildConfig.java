214fbdbe566532fecc32cd9785c5289365cd073a
frdisfafa <fnuraidah990@gmail.com>
2021-01-07 17:28:05 +0700

    from favorite_user branch commit

---
+++ consumerapp/build/generated/source/buildConfig/debug/com/faridaaidah/consumerapp/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.faridaaidah.consumerapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.faridaaidah.consumerapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GITHUB_TOKEN = "d8f1963ca4c782a251e0b31b42cd09b511a131b5";
+}
